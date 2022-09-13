6056edb6d0c584601bf1bf3f8d88ca95b7fd23d1
Furkan DUMANLI <66632576+dmnlfrkn@users.noreply.github.com>
2022-09-13 19:46:49 +0300

    Update RepositoryDal.java

--- src/main/java/com/patika/project/firstCase/DataAccess/RepositoryDal.java
+++ src/main/java/com/patika/project/firstCase/DataAccess/RepositoryDal.java
@@ -79,35 +79,4 @@ public class RepositoryDal implements IRepositoryDal{
 				.getResultList();
 		return products;
 	}
-	/*
-	@Override
-	public void add(City city) {
-		// if(id)
-		Session session = entityManager.unwrap(Session.class);
-		session.saveOrUpdate(city);
-		
-	}
-
-	@Override
-	public void update(City city) {
-		Session session = entityManager.unwrap(Session.class);
-		session.saveOrUpdate(city);
-		
-	}
-
-	@Override
-	public void delete(City city) {
-		Session session = entityManager.unwrap(Session.class);
-		City cityToDelete = session.get(City.class, city.getId());
-		session.delete(cityToDelete);
-		
-	}
-
-	@Override
-	public City getById(int id) {
-		Session session = entityManager.unwrap(Session.class);
-		City city = session.get(City.class, id);
-		return city;
-	}*/
-
 }
